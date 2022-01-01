8034b71696e94807d9430cc720b061cef26fc98e
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-01 17:59:54 +0800

    Add Sidebar Nav and styles

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { ListGroup, Nav, Navbar } from "react-bootstrap";
 import { sidebarWidth } from "../../Constants";
 import "./Sidebar.css";
+
 
 function Sidebar() {
     return (
         <div className="sidebar" style={{width: sidebarWidth}}> 
-            <div>List1</div>
-            <div>List2</div>
+                <Nav variant="pills" defaultActiveKey="cat1" className="flex-column" onSelect={(key)=> console.log(key)}>
+                    <Nav.Item> 
+                        <Nav.Link eventKey="cat1" className="text-white">Cat 1</Nav.Link>
+                    </Nav.Item>
+
+                    <Nav.Item>
+                        <Nav.Link eventKey="cat2" className="text-white">Cat 2</Nav.Link>
+                    </Nav.Item>
+                </Nav>
         </div>
     )
 }
