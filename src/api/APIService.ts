523aefdcb176ce74b0da6e451f791de73047eb70
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-03 23:13:31 +0800

    Add addCategory method to APIService

--- src/api/APIService.ts
+++ src/api/APIService.ts
@@ -3,14 +3,19 @@ import axios from 'axios';
 
 function APIService() {
     const { REACT_APP_API_URL:API_URL } = process.env;
+    const CATEGORIES = API_URL + "categories";
 
     function delay(ms: number) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     async function getCategories(): Promise<Category[]> {
-        const url = API_URL + "categories";
-        const res = await axios.get<Category[]>(url);
+        const res = await axios.get<Category[]>(CATEGORIES);
+        return res.data;
+    }
+
+    async function addCategory(name: string): Promise<Category> {
+        const res = await axios.post<Category>(CATEGORIES, { name });
         return res.data;
     }
 
