eb76a7681f408be4d68f576f14a39e336890e175
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-03 22:00:32 +0800

    Add Formik and basic Add Cat form

--- src/features/categories/AddCategory.tsx
+++ src/features/categories/AddCategory.tsx
@@ -1,7 +1,10 @@
 import { findAllByDisplayValue } from '@testing-library/react';
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal, Row } from 'react-bootstrap';
 import { PlusLg } from 'react-bootstrap-icons'
+//import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 function AddCategory() {
     const [show, setShow] = useState<boolean>(false);
@@ -18,6 +21,10 @@ function AddCategory() {
         setTimeout(() => setCanClose(true), 2000);
     }
 
+    const validation = Yup.object({
+        name: Yup.string().required("Category name can't be blank")
+    });
+
     return (
         <>
         <Button variant="secondary" className="mx-2 mt-4 d-flex justify-content-center align-items-center"
@@ -31,7 +38,44 @@ function AddCategory() {
                 <Modal.Title>Add Category</Modal.Title>
             </Modal.Header>
 
-            <Modal.Body> Modal Body</Modal.Body>
+            <Modal.Body> 
+                <Formik 
+                    initialValues={{
+                        name: ''
+                    }}
+                    validationSchema={validation}
+                    onSubmit={(values) => console.log("Add cat ", values)}
+                >
+                    {/* <Form>
+                        <label htmlFor="name">Category name: </label>
+                        <Field name="name" type="text"></Field>
+                        <ErrorMessage name="name" />
+                    </Form> */}
+
+                    {
+                        ({handleSubmit, handleChange, handleBlur, values, touched, isValid, errors}) => (
+                            <Form noValidate onSubmit={handleSubmit}>
+                                <Row>
+                                    <Form.Group>
+                                        <Form.Label>Name: </Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            name="name"
+                                            value={values.name}
+                                            onChange={handleChange}
+                                            isValid={touched.name && !errors.name}
+                                            isInvalid={!!errors.name}
+                                            onBlur={handleBlur}
+                                        />
+                                        <Form.Control.Feedback>Looks good</Form.Control.Feedback>
+                                        <Form.Control.Feedback type="invalid">Category name can't be blank</Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </Modal.Body>
 
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose} disabled={!canClose}>Cancel</Button>
