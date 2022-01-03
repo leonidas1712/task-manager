9040b9839d3abd8506db7dc3d29495c755877842
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-03 16:22:17 +0800

    Add selector for category page to get category by id

--- src/features/categories/CategoryPage.tsx
+++ src/features/categories/CategoryPage.tsx
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 import { Container, Button, Row, Modal, } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
+import { errorCategory, selectCategoryById } from "./categoriesSlice";
+
+
 interface CategoryProps {
    
 }
@@ -9,10 +13,15 @@ function CategoryPage(props:{}) {
     const params = useParams();
     const [show, setShow] = useState<boolean>(false);
 
+    const id = Number(params.categoryId);
+
+    let category = useAppSelector((state) => selectCategoryById(state, id));
+    category = category ? category : errorCategory();
+ 
     return (
         <Container>
             <div className="d-flex align-items-center">
-                <h1>Category: {params.categoryId}</h1>
+                <h1>Category: {category.name}</h1>
                 <Button variant="primary mx-3" onClick={() => setShow(true)}> Rename </Button>
                 <Button variant="danger"> Delete </Button>
 
