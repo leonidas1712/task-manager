679563c4b4f590c28f17bb935b8cb4a25286d767
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-03 20:27:18 +0800

    Add modal logic

--- src/features/categories/AddCategory.tsx
+++ src/features/categories/AddCategory.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import { findAllByDisplayValue } from '@testing-library/react';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { PlusLg } from 'react-bootstrap-icons'
+
 function AddCategory() {
+    const [show, setShow] = useState<boolean>(false);
+    // modal backdrop=true keyboard=true, closeButton=true, other btns disabled = false when modal is closable
+    // not closable: invert above properties, backdrop="static"
+    const [canClose, setCanClose] = useState<boolean>(true);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const disableClose = () => {
+        setCanClose(false);
+
+        setTimeout(() => setCanClose(true), 2000);
+    }
+
     return (
-        <Button variant="secondary" className="mx-2 mt-4 d-flex justify-content-center align-items-center">
+        <>
+        <Button variant="secondary" className="mx-2 mt-4 d-flex justify-content-center align-items-center"
+        onClick={handleShow}>
             <PlusLg size={20} style={{marginRight: "0.5rem"}}/>
             <span className="lead">Add Category</span>
         </Button>
+
+        <Modal show={show} onHide={handleClose} backdrop={canClose ? canClose : "static"} keyboard={canClose}>
+            <Modal.Header closeButton={canClose}>
+                <Modal.Title>Add Category</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body> Modal Body</Modal.Body>
+
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose} disabled={!canClose}>Close</Button>
+                <Button variant="primary" onClick={handleClose} disabled={!canClose}> Save Changes</Button>
+                <Button variant="danger" onClick={disableClose}>Disable close</Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
