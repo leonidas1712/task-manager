46ae113bfffa7357f423812fd1af9c70cb325cd2
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-03 18:28:24 +0800

    Remove some comments

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -8,32 +8,6 @@ import "./Sidebar.css";
 import { Category } from '../../Types';
 import CategoryList from "../categories/CategoryList";
 
-
-
-interface CategoryData {
-    name:string;
-    route:string;
-}
-
-function getCategoryData(): CategoryData[] {
-    return [
-        {
-            name: "Inbox",
-            route:"1"
-        },
-
-        {
-            name: "Cat1",
-            route:"2"
-        },
-
-        {
-            name: "Cat2",
-            route:"3"
-        }
-    ]
-}
-
 function CategoryNav(props: {name: string; route: string, setActive:React.Dispatch<React.SetStateAction<string>> }) {
     return (
         <div className="d-inline-flex align-items-center justify-content-between">
@@ -52,9 +26,6 @@ function datumToCategoryNav(datum: Category, setActive:React.Dispatch<React.SetS
     return <CategoryNav key={datum.id} name={datum.name} route={datum.id+""} setActive={setActive}/>
 }
 
-// function datumToCategoryNav(datum: Category, setActive:React.Dispatch<React.SetStateAction<string>>) {
-//     return <CategoryNav key={datum.id} name={datum.name} route={datum.id} setActive={setActive}/>
-// }
 
 function Sidebar() {
     const navigate = useNavigate();
@@ -65,20 +36,9 @@ function Sidebar() {
     // every refresh it navigates to the active URL
     // TODO: change navigation logic to be through updating active category ID in redux store
     useEffect(() => {
-        const data = getCategoryData();
         navigate("categories/" + active);
 
     }, [active])
-
-    // const onSelect = (key: string | null) => {
-    //     if (key == null) {
-    //         console.log("Key was null");
-    //     } else {
-    //         console.log(key);
-    //         navigate("categories/" + key);
-    //         //setActive(key);
-    //     }
-    // };
 
     return (
         <div className="sidebar" style={{width: sidebarWidth}}> 
@@ -90,13 +50,9 @@ function Sidebar() {
                      <h3 className="text-center mt-2">Task Manager</h3>
                      <div className="mt-3"></div>
 
-                    {/* {data.length === 0 ? "Loading" : datumToCategoryNav(data[0], setActive)} */}
                     <CategoryList categories={allCategories} setActive={setActive} />
 
-                    {/* {data.slice(1).map((datum) => datumToCategoryNav(datum, setActive)) } */}
                 </Nav>
-
-                {/* {allCategories.length == 0 ? Spin : allCategories.toString()} */}
         </div>
     )
 }
