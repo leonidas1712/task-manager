a5851b535a91b14679918b8f3e72a6ebad414c81
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-04 01:31:49 +0800

    Move sortComparer (earliest first) into Constants

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -1,1 +1,10 @@
+import { CreatedAt } from "./Types";
+
 export const sidebarWidth = "15rem";
+
+// earliest created comes first - default sort comparer for keeping in Redux store
+// JS allows date subtraction but typescript needs numeric values
+export const sortComparer = (fst:CreatedAt, snd:CreatedAt) => {
+    return new Date(fst.created_at).getTime() - new Date(snd.created_at).getTime()
+};
+
