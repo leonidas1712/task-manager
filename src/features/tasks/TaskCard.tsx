a8de5f923bd68e1bf9cb031eef539180068c899d
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-04 15:50:47 +0800

    Add task delete to task card

--- src/features/tasks/TaskCard.tsx
+++ src/features/tasks/TaskCard.tsx
@@ -1,14 +1,28 @@
 //import { useAppSelector } from '../../app/hooks';
+import { useState } from 'react';
 import { Card, ToggleButton, FormCheck} from 'react-bootstrap';
 import { Task } from '../../Types';
 import Checkbox from '@mui/material/Checkbox';
 import { Tooltip } from '@mui/material';
+import { deleteTask } from './tasksSlice';
+import { useAppDispatch } from '../../app/hooks';
+
+
 
 //import { errorTask, selectTaskById } from './tasksSlice';
 
 
 type TaskCardProps = { task:Task }
 function TaskCard({ task }:TaskCardProps) {
+    const [checkboxDisabled, setCheckboxDisabled] = useState<boolean>(false);
+    const dispatch = useAppDispatch();
+
+    const checkboxDelete = async () => {
+        setCheckboxDisabled(true);
+        await dispatch(deleteTask(task.id));
+        console.log("Task deleted")
+    }
+
     return (
         <div>
             <Card className= "mb-3 border border-secondary shadow-sm">
@@ -16,7 +30,7 @@ function TaskCard({ task }:TaskCardProps) {
                     <Card.Title className="d-flex align-items-center"> 
                         <span> {task.name} </span>
                         <Tooltip title="Complete task" placement="right-start">
-                            <Checkbox/>
+                            <Checkbox onClick={checkboxDelete} disabled={checkboxDisabled}/>
                         </Tooltip>
                     </Card.Title>
                     <Card.Text>{task.description || "Some desc"} </Card.Text>
