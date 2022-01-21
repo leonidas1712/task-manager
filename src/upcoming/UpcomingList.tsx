c243c7dcf9ddfaef96533638485308a1ffcfd1dd
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-21 17:39:34 +0800

    Add sort by button to UpcomingCard

--- src/upcoming/UpcomingList.tsx
+++ src/upcoming/UpcomingList.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import useSortBy from "../features/categories/useSortBy";
 import { dateISOToDateDisplay } from "../features/common/dateObjects";
 import TasksList from "../features/tasks/TasksList";
 import { Task } from "../Types"
@@ -7,12 +8,18 @@ import { Task } from "../Types"
 // pre filter tasks to avoid unnecc. repeated filtering
 function UpcomingCard(props: { dateDisplay:string, tasks: Task[]}) {
     const { dateDisplay, tasks } = props;
+    const { sortOption, SortByButton } = useSortBy();
+
 
     return (
         <div>
-            <h3 className="fs-4">{dateDisplay}</h3>
+            <div className="d-flex align-items-center">
+                <h3 className="fs-4">{dateDisplay}</h3>
+                <SortByButton />
+            </div>
+            
             <hr></hr>
-            <TasksList tasks={tasks} showCategory/>
+            <TasksList tasks={tasks} showCategory sortBy={sortOption}/>
             <div className="mb-5"></div>
         </div>
     );
@@ -22,10 +29,10 @@ function UpcomingCard(props: { dateDisplay:string, tasks: Task[]}) {
 // output: object with key as due date ISO, value as array of tasks with that date
 type TaskGroupByDate = Record<string, Task[]>;
 function groupTasksByDueDate(tasks: Task[]): TaskGroupByDate {
-
     const dateToTasks: TaskGroupByDate = tasks.reduce((acc:TaskGroupByDate, curr:Task) => {
         let key = curr.due_date;
-        // convert to date str to get tasks with same date regardless of time
+        // convert to date display str to get tasks with same date regardless of time
+        // use it directly for displaying, no extra conversion req.
         key = key ? dateISOToDateDisplay(key) : undefined;
         if (key == undefined) { return acc; } // return immediately in case faulty due date
 
@@ -51,11 +58,12 @@ function groupTasksByDueDate(tasks: Task[]): TaskGroupByDate {
 function UpcomingList({ tasks }: { tasks: Task[] }) {
     const tasksWithDueDate:Task[] = tasks.filter(t => t.due_date != null);
 
+
     const groupToCards = () => {
         const group = groupTasksByDueDate(tasksWithDueDate);
 
         return Object.keys(group).map((key) => 
-            <UpcomingCard key={key} dateDisplay={key} tasks={group[key]}/>
+            <UpcomingCard key={key} dateDisplay={key} tasks={group[key]} />
         );
     }
 
