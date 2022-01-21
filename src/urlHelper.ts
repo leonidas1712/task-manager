aaf8865bf5330d7f71462f19f710247a2c2259c7
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-21 12:19:22 +0800

    Change urlHelper.ts

--- src/urlHelper.ts
+++ src/urlHelper.ts
@@ -1,5 +1,4 @@
 import { useLocation, useNavigate } from "react-router-dom";
-//import { BASE_URL } from "./Constants";
 export const CATEGORIES_PATH = "categories";
 export const UPCOMING_PATH = "upcoming";
 export const ALL_TASKS_PATH = "all";
@@ -12,7 +11,6 @@ const isId = (url:string | number):boolean => {
 // take in url location.pathname from useLocation and return the page id (upcoming or category id)
 // format: /BASE_URL/:id e.g '/categories/113', page id might be blank
 const pageIdFromUrl = (url:string):string => {
-    //const start = `/${CATEGORIES_PATH}/`;
     // use split on "/" to get the very last part of the url: either a string like upcoming or a numeric id
     const arr =  url.split("/");
     return arr[arr.length - 1];
@@ -30,14 +28,12 @@ export function useNavigateHelper() {
     const navigate = useNavigate();
 
     function customNavigate(path: string | number | null) {
-        console.log(path);
         if (path == null) {
             return navigate("");
         }
 
         if (isId(path)) {
             const url = `${CATEGORIES_PATH}/${path}`;
-            console.log(url);
             return navigate(url);
         }
 
